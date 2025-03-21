a52010c72df78aba5d54fb3623e6bee11d56db66
vinodbhatt83 <26417101+vinodbhatt83@users.noreply.github.com>
2025-03-21 01:23:21 -0400

    s3

--- lib/s3.ts
+++ lib/s3.ts
@@ -2,9 +2,9 @@ import AWS from 'aws-sdk';
 
 // Configure AWS SDK
 AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION,
+    accessKeyId: process.env.S3_ACCESS_KEY,
+    secretAccessKey: process.env.S3_SECRET_KEY,
+    region: process.env.S3_REGION,
 });
 
 // Create S3 service object
